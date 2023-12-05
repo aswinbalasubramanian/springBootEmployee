f6f0c67b43f530b9079ee8577080c43221d3ea03
Aswin <aswin.balasubramanian@agnity.com>
2023-12-05 06:57:06 +0530

    default spring bean scope is singleton

--- src/main/java/com/aswin/spring/springCore/innerbeans/Test.java
+++ src/main/java/com/aswin/spring/springCore/innerbeans/Test.java
@@ -5,10 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/aswin/spring/springCore/innerbeans/config.xml");
 		Employee employee =  (Employee)context.getBean("employee");
+		Employee employee2 =  (Employee)context.getBean("employee");
+		
 		System.out.println(employee);
+		System.out.println(employee.hashCode());
+		System.out.println(employee2);
+		System.out.println(employee2.hashCode());
 				
 	}
 
