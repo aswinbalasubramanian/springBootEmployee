660bde65e842de88db06c684be366f97bdf0382d
Aswin <aswin.balasubramanian@agnity.com>
2023-12-09 06:45:28 +0530

    constructor arg string ambiguity

---
+++ src/main/java/com/aswin/spring/springCore/constructorInjection/ambiguity/Addition.java
@@ -0,0 +1,17 @@
+package com.aswin.spring.springCore.constructorInjection.ambiguity;
+
+public class Addition {
+
+	Addition(double a, double b) {
+		System.out.println("Inside constructor DOUBLE");
+	}
+
+	Addition(int a, int b) {
+		System.out.println("Inside constructor INT");
+	}
+
+	Addition(String a, String b) {
+		System.out.println("Inside constructor STRING");
+	}
+
+}
---
+++ src/main/java/com/aswin/spring/springCore/constructorInjection/ambiguity/Test.java
@@ -0,0 +1,14 @@
+package com.aswin.spring.springCore.constructorInjection.ambiguity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/aswin/spring/springCore/constructorInjection/ambiguity/config.xml");
+		Addition add = (Addition)context.getBean("addition");
+		System.out.println(add);
+	}
+}
